fd27cd20ad872ca19dc67fa2203f7f83454ff937
Livingstone17 <peterson.omobolaji@gmail.com>
2022-01-11 13:03:03 +0100

    set up basic structure and styles

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+// Get the container element
+var linkContainer = document.querySelector(".post-nav");
+
+// Get all buttons with class="btn" inside the container
+var links = linkContainer.getElementsByClassName("post-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
